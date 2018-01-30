4464a9259fe2d818c70ea860ce10761cc158f797
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-29 20:21:00 -0600

    Add basic app

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config;
+const snoowrap = require('snoowrap');
+const snoostorm = require('snoostorm');
+let cred = { // Short for constructor credentials
+  id: process.env.CLIENT_ID,
+  secret: process.env.CLIENT_SECRET,
+  usr: 'Spaceface16518',
+  pass: 'redditbot'
+}
+const wrap = new snoowrap({
+  userAgent: 'Halo-Strat-Collector',
+  clientId: cred.id,
+  clientSecret: cred.secret,
+  username: cred.usr,
+  password: cred.pass
+
+})
+
+const client = new snoostorm(wrap);
+
+const queryOptions = {
+  subreddit: 'Halo',
+  results: 25, // Number of results collected per query
+}
+
+const streamPost = client.SubmissionStream(queryOptions)
+
+streamPost.on('post', (post) => {
+  console.log(post) // NOTE: right now just log to console, but this is where the post will be processed.
+})
