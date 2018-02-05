f671a5c1618bdea2f82480d65e82dbde4f9cd9aa
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-05 16:29:30 -0600

    Correct build paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Initiating Build..."
 echo "Building App..."
-tsc *.ts # NOTE: This directory is not relative to this file, it is relative to package.json
+tsc ./app.ts # NOTE: This directory is not relative to this file, it is relative to package.json
 echo "App built"
 echo "Building Modules..."
-tsc modules/*.ts
+tsc ./modules/proccess.ts # NOTE: process module
 echo "Modules Built"
 echo "Done"
