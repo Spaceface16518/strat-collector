c2aaa9eaca8b6e118c4dcdb5f2d29782586a3622
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-04 20:37:08 -0600

    Change checkpoint logging for better description

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Building..."
+echo "Initiating Build..."
 echo "Building App..."
 tsc *.ts # NOTE: This directory is not relative to this file, it is relative to package.json
 echo "App built"
