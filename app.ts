a7e96ba1ced3d5f947903749fbc6f5c336b44dbc
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-04 23:00:39 -0600

    Improve functionality of process module
    Tested

--- app.ts
+++ app.ts
@@ -1,35 +1,45 @@
-require('dotenv').config();
-const proc = require('./modules/process')
-const snoowrap = require('snoowrap');
-let cred = { // Short for constructor credentials
+require("dotenv").config();
+const snoowrap = require("snoowrap");
+const proc = require("./modules/process.js");
+function printNoNewline(text: string): void {
+  process.stdout.write(text);
+}
+
+printNoNewline("configuring...");
+let cred: object = {
+  // Short for constructor credentials
   id: process.env.CLIENT_ID,
   secret: process.env.CLIENT_SECRET,
   usr: process.env.USERNAME,
   pass: process.env.PASSWORD
-}
+};
+console.log("done");
+
+printNoNewline("accessing reddit...");
 const r = new snoowrap({
-  userAgent: 'Halo-Strat-Collector',
+  userAgent: "Halo-Strat-Collector",
   clientId: cred.id,
   clientSecret: cred.secret,
   username: cred.usr,
   password: cred.pass
+});
+console.log("done");
 
-})
-
+printNoNewline("setting query options...");
 const queryOptions: object = {
-  subreddit: 'Halo_Strat_Roulette',
-  results: 1, // Number of results collected per query
-}
+  subreddit: "Halo_Strat_Roulette",
+  results: 1 // Number of results collected per query
+};
+console.log("done");
 
-function getTags(submission: object): string[] {}
-
-console.log('Running...')
-r.getSubreddit("Halo_Strat_Roulette")
+printNoNewline("Collecting posts...");
+r
+  .getSubreddit("Halo_Strat_Roulette")
   .getTop({ time: "all" })
   .then(allSubmissions => {
-    for (let i = 0; i < allSubmissions.length; i++) {
-      const submission = allSubmissions[i];
-      console.log(submission.title);
-      getTags(submission);
+    for (let i: number = 0; i < allSubmissions.length; i++) {
+      const submission: object = allSubmissions[i];
+      let data: any = proc.processPost(submission);
     }
   });
+console.log("Done");
--- modules/process.ts
+++ modules/process.ts
@@ -1,15 +1,17 @@
-module.exports = function processPost(post:object): object{
-  // This function returns an *object* of then necessary data that needs to be put into the database
-  let data = {
-    title: post.title,
-    body: post.selftext,
-    author: post.author.name,
-    tags: this.getTags(),
-    getTags(): string[]{
-      let body: string= this.body;
-      let tagArray: string[] = body.split('|')
-      return tagArray
+module.exports.processPost = function(post: any): object {
+  // This function returns an object of then necessary data that needs to be put into the database
+  let data = {};
+  data.title = post.title;
+  data.author = post.author.name;
+  data.body = post.selftext;
+  data.tags = getTags(data.body);
+
+  function getTags(text: string): string[] {
+    if (text !== null || (text !== undefined && text.includes("|") === true)) {
+      return text.split("|");
+    } else {
+      return null;
     }
-  };
-  return data
-}
+  }
+  return data;
+};
