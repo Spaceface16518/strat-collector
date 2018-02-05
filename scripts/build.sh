e47d839fabe5427e1e6ba029abec93e4c6fcd35c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-04 19:59:42 -0600

    Add build script for modules

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Building..."
-tsc app.ts # NOTE: This directory is not relative to this file, it is relative to package.json
+echo "Building App..."
+tsc *.ts # NOTE: This directory is not relative to this file, it is relative to package.json
+echo "App built"
+echo "Building Modules..."
+tsc modules/*.ts
+echo "Modules Built"
 echo "Done"
