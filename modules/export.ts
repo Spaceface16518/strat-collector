515f820db6bf9d847cb1a3a416e6bdd18a933a89
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-02-06 19:56:41 -0600

    Patch export module

--- modules/export.ts
+++ modules/export.ts
@@ -1,11 +1,12 @@
 module.exports.preview = (data: any): string => {
-  let str = `Post by ${data.author}! It is called ${data.title}. Here's a snippet:\n${snip(data.body)}\n`;
+  let str = `Post by ${data.author}! It is called \"${data.title}\". Here's a snippet:\n${snip(data.body)}\n`;
+
   function snip(text: string) {
-    // Gets the first 50 characters of a line of text
+    // Gets the first 20 characters of a line of text
     if (text !== null || text !== undefined) {
       let characterArray: string[] = text.split("");
       let snippetArray: string[] = [];
-      for (let i = 0; i < 50; i++) {
+      for (let i = 0; i < 20; i++) {
         snippetArray.push(characterArray[i]);
       }
       let snippet: string = snippetArray.join("");
@@ -14,9 +15,25 @@ module.exports.preview = (data: any): string => {
       return null;
     }
   }
-  console.log(str)
+  return str
 };
 
-module.exports.spit = (data: any) => {
-  console.log(data);
+module.exports.spit = (data: any): string => {
+  let str = `\n${data.title} by ${data.author}\n${showIfString(data.body)}\nTags: ${data.tags}\nA net upvote value of ${data.netUps}\nThe object model is:\n${data}`;
+  
+  function showIfString(text: any){
+    if(isString(text) === true){
+      return text;
+    } else {
+      return null;
+    }
+    function isString(text: any) {
+      if(typeof text === 'string') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  return str;
 };
